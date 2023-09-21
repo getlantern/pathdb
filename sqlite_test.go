262952a16f9d8afbbceaaf651e085fce713d482d
Ox Cart <ox.to.a.cart@gmail.com>
2023-09-20 21:07:16 -0500

    Made core tests available to clients of pathdb

--- sqlite_test.go
+++ sqlite_test.go
@@ -2,16 +2,18 @@ package pathdb
 
 import (
 	"database/sql"
+	"path/filepath"
+	"testing"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/require"
 
 	"github.com/getlantern/pathdb/minisql"
 )
 
-func newSQLiteImpl(file string) (minisql.DB, error) {
-	db, err := sql.Open("sqlite3", file)
-	if err != nil {
-		return nil, err
-	}
-	return &minisql.DBAdapter{DB: db}, nil
+func newSQLiteImpl(t *testing.T) minisql.DB {
+	tmpDir := t.TempDir()
+	db, err := sql.Open("sqlite3", filepath.Join(tmpDir, "test.db"))
+	require.NoError(t, err)
+	return &minisql.DBAdapter{DB: db}
 }
